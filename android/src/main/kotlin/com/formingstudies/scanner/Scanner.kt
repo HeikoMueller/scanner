357cd9f73cb7e3195fdcd03c89055c29ccafe38e
Heiko Müller <heiko.aime@googlemail.com>
2020-06-24 14:13:42 +0200

    start android implementation

---
+++ android/src/main/kotlin/com/formingstudies/scanner/Scanner.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2020. Heiko Müller.
+ * All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
+ */
+
+package com.formingstudies.scanner
+
+import android.bluetooth.le.BluetoothLeScanner
+
+
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.AdvertiseCallback
+import android.bluetooth.le.AdvertiseData
+import android.bluetooth.le.AdvertiseSettings
+import android.bluetooth.le.BluetoothLeAdvertiser
+import android.content.Context
+import android.os.ParcelUuid
+import io.flutter.Log
+import android.bluetooth.BluetoothGattServerCallback
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGattService
+import android.bluetooth.BluetoothGattServer
+
+class Scanner {
+
+    private var mBluetoothLeScanner: BluetoothLeScanner = null
+
+
+    fun init(context: Context) {
+        if (mBluetoothLeScanner == null) {
+            mBluetoothLeScanner = (context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager).getBluetoothLeScanner()
+        }
+    }
+    fun start() {
+        Log.i(tag, "ANDROID SCANNER START")
+    }
+    fun stop() {
+        Log.i(tag, "ANDROID SCANNER STOP")
+    }
+}
--- android/src/main/kotlin/com/formingstudies/scanner/ScannerPlugin.kt
+++ android/src/main/kotlin/com/formingstudies/scanner/ScannerPlugin.kt
@@ -9,6 +9,8 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
+import io.flutter.plugin.common.FlutterEventChannel
+
 /** ScannerPlugin */
 public class ScannerPlugin: FlutterPlugin, MethodCallHandler {
   /// The MethodChannel that will the communication between Flutter and native Android
@@ -16,10 +18,19 @@ public class ScannerPlugin: FlutterPlugin, MethodCallHandler {
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
+  private lateinit var eventChannel : FlutterEventChannel
+
+  private var scanner: Scanner? = null
+
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "scanner")
+    channel = MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "roktok.immu.dev/bluetoothScanner")
     channel.setMethodCallHandler(this);
+    eventChannel = FlutterEventChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "roktok.immu.dev/bluetoothScannerResponse")
+    eventChannel.setMethodCallHandler(this);
+    scanner = Scanner()
+    scanner!!.init(applicationContext)
+
   }
 
   // This static function is optional and equivalent to onAttachedToEngine. It supports the old
@@ -34,20 +45,41 @@ public class ScannerPlugin: FlutterPlugin, MethodCallHandler {
   companion object {
     @JvmStatic
     fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "scanner")
+      val channel = MethodChannel(registrar.messenger(), "roktok.immu.dev/bluetoothScanner")
       channel.setMethodCallHandler(ScannerPlugin())
+      val eventChannel = MethodChannel(registrar.messenger(), "roktok.immu.dev/bluetoothScannerResponse")
+      eventChannel.setMethodCallHandler(ScannerPlugin())
     }
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
     if (call.method == "getPlatformVersion") {
       result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
+    } else if (call.method == "startScanning") {
+      Log.i(tag, "ANDROID startScanning called")
+      startScanning(result)
+    } else if (call.method == "stopScanning") {
+      Log.i(tag, "ANDROID stopScanning called")
+      stopScanning(result)
+    }
+    else {
       result.notImplemented()
     }
   }
 
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
+    eventChannel.setMethodCallHandler(null)
   }
+
+  private fun startScanning(result: MethodChannel.Result) {
+    scanner!!.start()
+    result.success(null)
+  }
+
+  private fun stopScanning(result: MethodChannel.Result) {
+    scanner!!.stop()
+    result.success(null)
+  }
+
 }
