ff31b5bee5652b3d700a0efaf5275af9e0e4fc8a
Heiko Müller <heiko.aime@googlemail.com>
2020-07-29 17:00:54 +0200

    try to implement android able coroutines library

--- android/src/main/kotlin/com/formingstudies/scanner/Scanner.kt
+++ android/src/main/kotlin/com/formingstudies/scanner/Scanner.kt
@@ -6,27 +6,20 @@
 
 package com.formingstudies.scanner
 
-import android.bluetooth.le.BluetoothLeScanner
-
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGatt
 import android.bluetooth.BluetoothManager
-// import android.bluetooth.le.AdvertiseCallback
-// import android.bluetooth.le.AdvertiseSettings
-// import android.bluetooth.le.BluetoothLeAdvertiser
+import android.bluetooth.le.*
 import android.content.Context
+import android.os.Build
 import android.os.ParcelUuid
-import io.flutter.Log
-// import android.bluetooth.BluetoothGattServerCallback
-// import android.bluetooth.BluetoothDevice
-// import android.bluetooth.BluetoothGattService
-// import android.bluetooth.BluetoothGattServer
+import android.util.Log
+import com.juul.able.experimental.ConnectGattResult
+import com.juul.able.experimental.android.connectGatt
+import com.juul.able.experimental.toUuid
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
-import android.bluetooth.le.ScanCallback
-import android.bluetooth.le.ScanFilter
-import android.bluetooth.le.ScanSettings
-import android.bluetooth.le.ScanResult
-import android.os.*
-// import android.os.Build.VERSION
-// import android.os.Build.VERSION_CODES
 
 class Scanner {
 
@@ -34,6 +27,10 @@ class Scanner {
     private var mBluetoothLeScanner: BluetoothLeScanner? = null
     private var scanCallback: ScanCallback? = null
     private var context: Context? = null
+
+    val serviceUuid = "0000FF01-0000-1000-8000-00805F9B34FB".toUuid()
+    val characteristicUuid_A = "08590F7E-DB05-467E-8757-72F6FAEB13D4".toUuid()
+    val characteristicUuid_B = "E20A39F4-73F5-4BC4-A12F-17D1AD07A961".toUuid()
 
 
     // private val apiLevel: Int = android.os.Build.VERSION.SDK_INT
@@ -45,7 +42,37 @@ class Scanner {
             mBluetoothLeScanner = (context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager).adapter.bluetoothLeScanner
         }
     }
-    
+
+    fun connect(context: Context, device: BluetoothDevice) = GlobalScope.launch {
+        val gatt = device.connectGatt(context, autoConnect = false).let { result ->
+            when (result) {
+                is ConnectGattResult.Success -> result.gatt
+                is ConnectGattResult.Canceled -> throw IllegalStateException("Connection canceled.", result.cause)
+                is ConnectGattResult.Failure -> throw IllegalStateException("Connection failed.", result.cause)
+            }
+        }
+
+        if (gatt.discoverServices() != BluetoothGatt.GATT_SUCCESS) {
+            // discover services failed
+        }
+
+        val characteristic = gatt.getService(serviceUuid)?.getCharacteristic(characteristicUuid_B)
+
+        val result = characteristic?.let { gatt.readCharacteristic(it) }
+        if (result != null) {
+            if (result.status == BluetoothGatt.GATT_SUCCESS) {
+                Log.i(TAG, "result.value = ${result.value}")
+            } else {
+                // read characteristic failed
+                Log.i(TAG, "ELSE CASE")
+            }
+        }
+
+        gatt.disconnect()
+        gatt.close()
+    }
+
+
 //    fun start(uuids: Array<String>) {
     fun startScanning(data: Data) {
         Log.i(TAG, "ANDROID SCANNER START")
@@ -71,7 +98,6 @@ class Scanner {
     }
 
     private val mScanCallback = object : ScanCallback() {
-
         override fun onScanResult(callbackType: Int, result: ScanResult) {
             super.onScanResult(callbackType, result);
             Log.i(TAG, result?.toString());
@@ -81,7 +107,6 @@ class Scanner {
     private fun buildScanSettings(): ScanSettings {
         val builder = ScanSettings.Builder()
         builder.setScanMode(ScanSettings.SCAN_MODE_LOW_POWER);//SCAN_MODE_LOW_LATENCY
-        builder.setMatchMode(ScanSettings.MATCH_MODE_AGGRESSIVE)
         return builder.build();
     }
 
