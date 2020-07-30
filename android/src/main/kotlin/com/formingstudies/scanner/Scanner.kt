9073017f250b5c3cac34d08b73c90bc06c6c7c8d
Heiko Müller <heiko.aime@googlemail.com>
2020-07-30 23:09:16 +0200

    try to implement device

--- android/src/main/kotlin/com/formingstudies/scanner/Scanner.kt
+++ android/src/main/kotlin/com/formingstudies/scanner/Scanner.kt
@@ -8,6 +8,7 @@ package com.formingstudies.scanner
 
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGattCharacteristic
 import android.bluetooth.BluetoothManager
 import android.bluetooth.le.BluetoothLeScanner
 import android.bluetooth.le.ScanCallback
@@ -21,7 +22,9 @@ import android.util.Log as mLog
 import com.juul.able.experimental.ConnectGattResult
 import com.juul.able.experimental.android.connectGatt
 import com.juul.able.experimental.toUuid
+import com.juul.able.experimental.Device
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 class Scanner {
@@ -38,7 +41,7 @@ class Scanner {
     val characteristicUuid_A = "08590F7E-DB05-467E-8757-72F6FAEB13D4".toUuid()
     val characteristicUuid_B = "E20A39F4-73F5-4BC4-A12F-17D1AD07A961".toUuid()
 
-
+    val isActive = true
     // private val apiLevel: Int = android.os.Build.VERSION.SDK_INT
 
     fun init(context: Context) {
@@ -48,6 +51,29 @@ class Scanner {
             mBluetoothLeScanner = (context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager).adapter.bluetoothLeScanner
         }
     }
+
+    fun connectAndReadEveryMinute(
+            context: Context,
+            bluetoothDevice: BluetoothDevice,
+            characteristic: BluetoothGattCharacteristic
+    ): Job = launch {
+        while (isActive) {
+            bluetoothDevice.connect(context)
+            bluetoothDevice.discoverServices()
+
+            val value = bluetoothDevice.readCharacteristic(characteristic).value
+
+            try {
+                bluetoothDevice.disconnect()
+            } finally {
+                bluetoothDevice.close()
+            }
+
+            delay(60_000L)
+        }
+    }
+
+
 
     fun connect(context: Context, device: BluetoothDevice) = GlobalScope.launch {
         val gatt = device.connectGatt(context, autoConnect = false).let { result ->
